ca9496a70a0230cf74df7f7402d99c60a01010ac
BigglesDevs <103225055+BigglesDevs@users.noreply.github.com>
2026-04-19 14:50:54 +0100

    rework

--- bot/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-const isAdmin = (interaction) => {
-  const { PermissionFlagsBits } = require("discord.js");
-  return interaction.user.id === process.env.ADMIN_DISCORD_ID ||
-    interaction.memberPermissions?.has(PermissionFlagsBits.Administrator);
-};
-module.exports = { isAdmin };
